5ef11714bdb894c2215c14eab62bed2404c0aa71
Alexey <optycat@gmail.com>
2022-11-05 19:46:29 +0200

    first commit

---
+++ jsLections.js
@@ -0,0 +1,147 @@
+"use strict";
+
+//lection 8 объявление переменных
+/*
+let num = 5;
+const leftBorderWidth = 50;
+
+num = 10;
+console.log(num);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = 'str';
+
+{
+    let res = 10;
+}
+
+console.log(res);
+*/
+
+//lection 11 типы данных
+
+/*
+
+// ---- Primitives
+
+numbers         1, 2, 500000000
+NaN             not a number
+
+string          'jgoisjregs'  "156"  `re14y56h`
+
+boolean         true/false
+
+null            Referense error
+undefined       
+
+Symbol
+Bigint
+
+// ---- Objects
+
+arrays          [1, 'gdf', null]
+
+
+               0        1     2     3    4   5
+let arr = ['fsdefh', 'srdgg', 4, false, {}, []];
+    console.log(arr[2]);
+
+//___________________//
+
+objects         { age: 50,  name: 'Sarah', isMarryed: false }
+
+
+const person= { 
+                age: 50,  
+                name: 'Sarah', 
+                isMarryed: false 
+            }
+
+    console.log(person.age);
+    console.log(person['name']);
+
+//___________________//
+
+*/
+
+//lection 12 работа с объектами
+
+/*
+
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    3: 'c',
+    abc: {
+        def: {
+            a: 4
+        },
+        fff: [{}, {}]
+    }
+};
+
+const d = 'd';
+
+
+arrObj.b = '1234';
+arrObj['c'] = '4321';
+arrObj[d] = '1234';
+
+*/
+
+//lection 13 работа с юзером
+
+/*
+
+alert('something');
+
+const result = confirm('R u here?');
+console.log(result);
+
+let ansver = prompt("R u adult?", "");
+    ansver = +prompt("R u adult?", "18");
+console.log(ansver);
+console.log(typeof(ansver));    //string
+
+console.log(ansver + 5);
+
+const ansvers = [];
+
+answers[0] = prompt('name', '');
+answers[1] = prompt('age', '');
+answers[2] = prompt('sername', '');
+
+document.write(ansvers);
+
+console.log(typeof(ansvers));   //object
+console.log(typeof(null));      //object
+
+*/
+
+//lection 14 интерполяция
+
+/* 
+
+const category = 'toys';
+
+console.log("dweytjrtfyuj/" + category);
+console.log(`dweytjrtfyuj/${category}`);
+
+*/
+
+//lection 14 операторы js
+
+/* 
+
+*/
+
+
