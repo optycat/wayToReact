12dfc2beb43f5b7c239489294ea11d5c536414ff
Alexey <optycat@gmail.com>
2022-11-08 22:29:23 +0200

    lecture update

--- jsLections.js
+++ jsLections.js
@@ -523,3 +523,99 @@ arr.forEach( (itemOfArr, indexOfItem, arr) => {
 
 
 */
+
+// lection 35 передача по ссылке и по значению spread operator
+
+/*
+
+
+let a = 5;
+let b = a;
+
+b = b + 5;
+
+console.log(a, b);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj;                   creates link to the obj
+copy.a = 50;
+
+console.log(obj, copy);
+
+const copyObject = (mainObject) => {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObject) {
+        objCopy[key] = mainObject[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 55,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newObj = copyObject(numbers);
+
+numbers.a = 10;
+newObj.c.x = 10;
+
+console.log(numbers, newObj);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add, clone);
+
+const oldArr = ['a', 'b', 'c'];
+const newArray = oldArr.slice();
+
+const video = ['tube', 'vimeo', 'netflix'],
+      blogs = ['hub', 'rezka', 'binance'],
+      internet = [...video, ...blogs, 'fasebook'];
+
+console.log(internet);
+
+function log(a, b, c){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const some = [1, 3, 5];
+
+log(...some);
+
+const arrrdaay = ['a', 'b'];
+
+const copyArrr = [...arrrdaay];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const nardlwfeaObj = {...q};
+
+
+*/
+
