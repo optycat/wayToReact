612b5c1dda2332b75f0870e45f51d5ea3b5a1825
Alexey <optycat@gmail.com>
2022-11-09 20:44:54 +0200

    lecture update

--- practice.js
+++ practice.js
@@ -1,87 +1,97 @@
 "use strict";
 
-let numberOfFilms = +prompt("How many films have you watched?", "");
-
-
+/*
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
-};
+    private: false,
+    start: () => {
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("How many films have you watched?", "");
+        }
+    },
+    writeYourGenre: () => {
+        let tmpGenreCount = 0;
 
+        while(tmpGenreCount < 3) {
+            let tmpGenre = prompt(`Enter your fawourite genre number ${tmpGenreCount + 1}:`, "");
+            personalMovieDB.genres[tmpGenreCount] = tmpGenre;
+            tmpGenreCount++;
 
-function start() {
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("How many films have you watched?", "");
-    }
-}
-
-const writeYourGenre = () => {
-    let tmpGenreCount = 0;
-
-    while(tmpGenreCount < 3) {
-        let tmpGenre = prompt(`Enter your fawourite genre number ${tmpGenreCount + 1}:`, "");
-        personalMovieDB.genres[tmpGenreCount] = tmpGenre;
-        tmpGenreCount++;
-
-        if(!tmpGenre) {
-            if(tmpGenreCount == 0) {
-                tmpGenreCount++;
+            if(!tmpGenre) {
+                if(tmpGenreCount == 0) {
+                    tmpGenreCount++;
+                }
+                tmpGenreCount--;
             }
-            tmpGenreCount--;
+        }
+
+        personalMovieDB.genres.forEach((item, index) => {
+            console.log(`Your genre favourite genre # ${index + 1}: ${item}\n`);
+        });
+    },
+    rememberFilm: () => {
+        for ( let i = 0; i < 2; i++) {
+    
+            let lastWathedFilm      = prompt("Name one of the last wathced films", ""),
+                lastWatchedFilmMark = +prompt("How it looks on scale 1 to 10?", "");
+        
+            if( !lastWathedFilm || lastWathedFilm.length > 49 ){
+                i--;
+            } else {
+                personalMovieDB['movies'][lastWathedFilm] = lastWatchedFilmMark;
+        
+                lastWathedFilm      = '';
+                lastWatchedFilmMark = 0;
+            }
+        
+        }
+    },
+    detectPersonalLvl: () => {
+        if ( personalMovieDB.count <= 10 ) {
+            console.log('a few films wathced');
+        } else if( personalMovieDB.count <= 30 ) {
+            console.log('regular viewier');
+        } else if ( personalMovieDB.count > 30 ) {
+            console.log('movie buff');
+        } else console.log('Error!');
+    },
+    toggleVisibleMyDb: () => {
+        if(personalMovieDB.private === false) personalMovieDB.private = true;
+        else personalMovieDB.private = false;
+    },
+    showMyDB: () => {
+        if(personalMovieDB.private == false) {
+            console.log(personalMovieDB);
         }
     }
-}
 
-function rememberFilm() {
-    for ( let i = 0; i < 2; i++) {
-    
-        let lastWathedFilm      = prompt("Name one of the last wathced films", ""),
-            lastWatchedFilmMark = +prompt("How it looks on scale 1 to 10?", "");
-    
-        if( !lastWathedFilm || lastWathedFilm.length > 49 ){
-            i--;
-        } else {
-            personalMovieDB['movies'][lastWathedFilm] = lastWatchedFilmMark;
-    
-            lastWathedFilm      = '';
-            lastWatchedFilmMark = 0;
-        }
-    
-    }
-}
-
-function detectPersonalLvl() {
-    if ( personalMovieDB.count <= 10 ) {
-        console.log('a few films wathced');
-    } else if( personalMovieDB.count <= 30 ) {
-        console.log('regular viewier');
-    } else if ( personalMovieDB.count > 30 ) {
-        console.log('movie buff');
-    } else console.log('Error!');
-}
-
-const showMyDB = (acsesToDB) => {
-    if(acsesToDB == false) {
-        console.log(personalMovieDB);
-    }
 };
 
 
-start();
 
-rememberFilm();
-detectPersonalLvl();
-writeYourGenre();
-showMyDB(personalMovieDB.private);
+
+
+
+personalMovieDB.start();
+
+personalMovieDB.rememberFilm();
+personalMovieDB.detectPersonalLvl();
+personalMovieDB.writeYourGenre();
+personalMovieDB.toggleVisibleMyDb();
+personalMovieDB.toggleVisibleMyDb();
+
+personalMovieDB.showMyDB();
 
 
 
 // some shablones
 
 /*
+
+
 function getCoupe(place) {
     let result;
     if(!place || typeof(place) != 'number' || place <= 0 || place > 36 || place % 1 != 0) {
@@ -218,8 +228,6 @@ function showProgrammingLangs(plan) {
 console.log(showProgrammingLangs(personalPlanPeter));
 
 
-*/
-
 const someString = 'This is some strange string';
 
 function reverse(str) {
@@ -230,7 +238,90 @@ console.log(reverse(someString));
 const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-function availableCurr(arr, missingCurr) {
+function availableCurr(arr, arr1, missingCurr) {
+    let result = 'Доступные валюты:\n';
+    if(!arr || !arr1) {
+        result = 'Нет доступных валют';
+    }
+    const arrayCurr = [...arr, ...arr1];
+    
+    for (let i = 0; i < arrayCurr.length; i++) {
+        result += `${arrayCurr[i]}\n`;
+        if(missingCurr) {
+            continue;
+        }
+    }
 
+    return result;
 }
 
+console.log();
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let value = 0,
+        result = '';
+    data.shops.forEach( (item) => {
+        const {width, length} = item;
+        value += width * length;
+    });
+    const prisePerM3 = value * data.height * data.moneyPer1m3;
+    if(data.budget >= prisePerM3) {
+        result += 'Бюджета достаточно';
+    } else result += 'Бюджета недостаточно';
+    return result;
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+
+*/
+
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Bernard', 'Sam', 'Sam'];
+
+let i = 0;
+    students.sort();
+
+function sortStudentsByGroups(arr, i, newArr = []) {
+
+    if(newArr.length === 3) {
+        let str = '';
+        if (arr.length === 9) {
+            str = `Оставшиеся студенты: -`;
+        } else {
+            arr = arr.slice(9);
+            str = `Оставшиеся студенты: ${arr.join(", ")}`;
+        }
+        newArr.push(str);
+        return newArr;
+    } else {
+        newArr.push([arr[i], arr[i + 1], arr[i + 2]]);
+        return sortStudentsByGroups(arr, i + 3, newArr);
+    }
+}
+console.log(sortStudentsByGroups(students, i));
