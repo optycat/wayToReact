b32d8c5b792dcccd20fcbd7543807a9631407133
Alexey <optycat@gmail.com>
2022-11-11 18:46:00 +0200

    lections update

--- script/jsLections.js
+++ script/jsLections.js
@@ -766,6 +766,83 @@ console.log(0 || "" || 2 || undefined || true || falsе );
 
 // lection 42 получение елементов со страницы
 
-/* */
+/*
 
+const box = document.getElementById('box');
+console.log(box);
+
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach( (item) => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+
+*/
+
+// lection 43 действие с елементами со страницы
+
+/*
+
+const wrapper  = document.querySelector('.wrapper'),
+      box      = document.getElementById('box'),
+      buttons  = document.getElementsByTagName('button'),
+      circles  = document.getElementsByClassName('circle'),
+      hearts   = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+
+box.style.backgroundColor = 'blue';
+box.style.width = '250px';
+
+const num = 500;
+box.style.cssText = `background-color: blue; width: ${num}px`;
+
+buttons[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+//for(let i = 0; i < hearts.length; i++) {
+//    hearts[i].style.backgroundColor = 'blue';
+//}
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+const text = document.createTextNode('Pres F to pay respect!');
+
+div.classList.add('black');
+
+wrapper.append(div);
+wrapper.prepend(div);
+
+wrapper.appendChild(div);
+
+hearts[0].before(div);
+hearts[0].after(div);
+
+wrapper.insertBefore(div, hearts[1]);
+
+circles[0].remove();
+wrapper.removeChild(hearts[1]);
+
+hearts[0].replaceWith(circles[0]);
+wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<p>Hi, there!</p>';
+
+div.textContent = 'Hi, biach!';
+
+div.insertAdjacentHTML('afterbegin', '<h2>height</h2>');
+
+*/
 
