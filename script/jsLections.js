585b9703e788035cae97001193e109230524c14a
Alexey <optycat@gmail.com>
2022-11-12 17:18:03 +0200

    practise and lections refactor

--- script/jsLections.js
+++ script/jsLections.js
@@ -1038,3 +1038,255 @@ console.log(factorial(0));
 // lection 47 рекурсия
 
 /* */
+
+
+
+// some shablones
+
+/*
+
+function getCoupe(place) {
+    let result;
+    if(!place || typeof(place) != 'number' || place <= 0 || place > 36 || place % 1 != 0) {
+        result = 'error!';
+    } else {
+        let counter = 1;
+        while( place >= 4) {
+            place = place - 4;
+            counter++;
+        }
+        result = 'done: ' + counter;
+    }
+
+    return result;
+}
+
+console.log(getCoupe(33));
+
+const transformNumberToDate = function(numberToTransform) {
+    const howersStr = ['час', 'часа', 'часов'];
+    let result = '';
+    if(!numberToTransform || typeof(numberToTransform) != 'number' || numberToTransform < 0 || numberToTransform % 1 != 0) {
+        console.log('error!');
+    } else {
+        let howers = 0;
+        while(numberToTransform >= 60) {
+            numberToTransform = numberToTransform - 60;
+            howers++;
+        }
+        result += howers;
+        if(howers % 10 < 2) {
+            result += howersStr[0];
+        } else if (howers % 10 < 5) {
+            result += howersStr[1];
+        } else {
+            result += howersStr[2];
+        }
+        result += ` ${numberToTransform} минут`;
+    }
+    return result;
+};
+
+console.log(transformNumberToDate(2656));
+
+const defineBiggest = function (a, b, c, d){
+    let result;
+    if(!a || typeof(a) != 'number' || !b || typeof(b) != 'number' || !c || typeof(c) != 'number' || !d || typeof(d) != 'number'){
+        result = 0;
+    } else {
+        result = (a > b ? a : b) > (c > d ? c : d) ? (a > b ? a : b) : (c > d ? c : d);
+    }
+    return result;
+};
+
+console.log(defineBiggest(1, 2, -3.5, -500));
+
+function fib(amount) {
+    let result = '',
+        resultArr = [0, 1];
+    if(typeof(amount) != 'number' || amount % 1 != 0 || amount < 0){
+        result = 'error!';
+    } else {
+        for(let i = 0; i < amount; i++) {
+            resultArr[i + 2] = resultArr[i] + resultArr[i + 1];
+            result += ` ${resultArr[i]}`;
+        }
+    }
+    return result.trim();
+}
+
+console.log(fib(undefined));
+
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: (object) => {
+        let responce = '';
+        if(object.age) {
+            responce += `My age is ${object.age} and `;
+        } else {
+            responce += `My age is empty and `;
+        }
+        if(object.skills.languages) {
+            responce += 'my languages are ';
+            for( let key in object.skills.languages) {
+                responce += `${object.skills.languages[key]} `.toUpperCase();
+            }
+        } else {
+            responce += `my languages are empty too`;
+        }
+        return responce;
+    }
+};
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+//const {skills} = personalPlanPeter;
+
+//console.log(skills);
+
+function showExperience(plan) {
+    //let someArr = [];
+    //for(let item in plan) {
+    //    if(typeof(plan[item]) === 'object') {
+    //        for(let i in plan[item]) {
+    //            if(typeof(plan[item][i]) !== 'object') {
+    //                someArr.push({i} = plan[item][i]);
+    //            }
+    //        }
+    //    }
+    //}
+    return plan.skills.exp;
+}
+
+console.log(showExperience(personalPlanPeter));
+
+function showProgrammingLangs(plan) {
+    let responce = '';
+    for( let key in plan.skills.programmingLangs) {
+        responce += `Language ${key} progress ${plan.skills.programmingLangs[key]} `;
+    }
+    return responce;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverse(someString));
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, arr1, missingCurr) {
+    let result = 'Доступные валюты:\n';
+    if(!arr || !arr1) {
+        result = 'Нет доступных валют';
+    }
+    const arrayCurr = [...arr, ...arr1];
+    
+    for (let i = 0; i < arrayCurr.length; i++) {
+        result += `${arrayCurr[i]}\n`;
+        if(missingCurr) {
+            continue;
+        }
+    }
+
+    return result;
+}
+
+console.log();
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let value = 0,
+        result = '';
+    data.shops.forEach( (item) => {
+        const {width, length} = item;
+        value += width * length;
+    });
+    const prisePerM3 = value * data.height * data.moneyPer1m3;
+    if(data.budget >= prisePerM3) {
+        result += 'Бюджета достаточно';
+    } else result += 'Бюджета недостаточно';
+    return result;
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takeshi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+
+    let result = [];
+    arr.sort();
+
+    for(let i = 0; i < 9; i += 3) {
+        result.push(arr.slice(i, i + 3));
+    }
+    arr.length > 9 ? result.push(`Оставшиеся студенты: ${arr.slice(9).join(", ")}`) : result.push('Оставшиеся студенты: -');
+
+    return result;
+    
+    //let i = 0;
+    //students.sort();
+    //function sortStudentsByGroups(arr, i, newArr = []){
+    //    if(newArr.length === 3) {
+    //        let str = '';
+    //        if (arr.length === 9) {
+    //            str = `Оставшиеся студенты: -`;
+    //        } else {
+    //            arr = arr.slice(9);
+    //            str = `Оставшиеся студенты: ${arr.join(", ")}`;
+    //        }
+    //        newArr.push(str);
+    //        return newArr;
+    //    } else {
+    //        newArr.push([arr[i], arr[i + 1], arr[i + 2]]);
+    //        return sortStudentsByGroups(arr, i + 3, newArr);
+    //    }
+    //}
+    //return ass(arr, i);
+    
+}
+console.log(sortStudentsByGroups(students));
+
+*/
